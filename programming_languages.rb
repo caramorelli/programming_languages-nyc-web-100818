a580b347e0a35c70a1cc5aa254e6fc762de15e22
Cara Morelli <caramorelli7@gmail.com>
2018-09-28 18:09:43 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,6 @@ def reformat_languages(languages)
   languages.each do |key, val|
     val.each do |k, v|
       lang_obj[k] = v.merge({:style => key.to_s})
-      
     end 
   end 
   puts lang_obj
